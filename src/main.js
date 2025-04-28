a1d45cf44b06b4129ab5a78d75b156a4e84ad809
ss585801 <ss585801@hotmail.com>
2025-04-28 20:15:03 +0800

    SASS降版本、依照期中考核進行分頁、登入註冊(功能還沒有)

--- src/main.js
+++ src/main.js
@@ -1,14 +1,12 @@
 import 'bootstrap/scss/bootstrap.scss'
-import './assets/main.css'
-import './scss/all.scss'
+import './assets/styles/all.scss'
 
 import * as bootstrap from 'bootstrap'
 
 import { createApp } from 'vue'
 
 import App from './App.vue'
+import router from './router'
+import './assets/main.scss'
 
-
-const app = createApp(App)
-
-app.mount('#app')
+createApp(App).use(router).mount('#app')
---
+++ src/router/index.js
@@ -0,0 +1,24 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import Header from '@/components/Header.vue'
+import LoginPage from '@/pages/LoginPage.vue'
+import RegisterPage from '@/pages/RegisterPage.vue'
+import ProfilePage from '@/pages/ProfilePage.vue'
+import Page1 from '@/pages/Page1.vue'
+import Page2 from '@/pages/Page2.vue'
+import HomePage from '@/pages/HomePage.vue'
+
+const routes = [
+  { path: '/', component: HomePage },
+  { path: '/login', component: LoginPage },
+  { path: '/register', component: RegisterPage },
+  { path: '/profile', component: ProfilePage },
+  { path: '/page1', component: Page1 },
+  { path: '/page2', component: Page2 }
+]
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes
+})
+
+export default router
